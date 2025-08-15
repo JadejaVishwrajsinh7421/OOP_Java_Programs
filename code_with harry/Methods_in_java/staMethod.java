4c7f5c02945b2c6d7b7c9c5f9066874def0f7ab4
JadejaVishwrajsinh7421 <JadejaVishwrajsinh7421>
2025-08-15 18:32:49 +0530

    "method demo in java."

--- code_with harry/Methods_in_java/staMethod.java
+++ code_with harry/Methods_in_java/staMethod.java
@@ -3,11 +3,25 @@ class Demo {
         System.out.println("Hello from static method!");
     }
 
+    
+    // non static method
+    void sayHi() { // Non-static method
+        System.out.println("Hello from non-static method!");
+    }
+
     public static void main(String[] args) {
         // Calling without object
-        Demo.greet();
+        // Demo.greet();
 
         // Or directly, since main is static and in the same class
         greet();
+
+         // Need to create an object
+        Demo obj = new Demo();
+        obj.sayHi();
     }
+       
+
 }
+
+// 
